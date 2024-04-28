13b5237eb0204f1e6aa118c6cfdd461c7f0df31f
basitng <basitng2004@gmail.com>
2024-04-28 16:01:13 +0100

    updated: latest version

--- src/utils/nanoid.ts
+++
@@ -1,8 +0,0 @@
-import { customAlphabet } from "nanoid/non-secure";
-
-export const nanoid = (chars?: number) => {
-  return customAlphabet(
-    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
-    chars || 7 // 7-character random string by default
-  )();
-};
